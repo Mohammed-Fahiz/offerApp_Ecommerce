3bce8c0e5e8d2eff7ea83aa4b9c55242cad5637d
Mohammed Fahiz <138813355+Mohammed-Fahiz@users.noreply.github.com>
2024-05-02 23:42:34 +0530

    Update mapscreen.dart

--- lib/screens/map/mapscreen.dart
+++ lib/screens/map/mapscreen.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart ';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
